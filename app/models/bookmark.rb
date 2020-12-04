787e88d3efe1f238649d68cc494e0b1d494f3628
chrismad26 <cmadison@chicagobooth.edu>
2020-12-04 03:52:18 +0000

    added the nav bar

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -9,4 +9,13 @@
 #  user_id    :integer
 #
 class Bookmark < ApplicationRecord
+  # def user
+  #   my_user_id = self.user_id
+
+  #   matching_users = User.where({ :id => my_user_id })
+
+  #   the_user = matching_users.at(0)
+
+  #   return the_user
+  # end
 end
